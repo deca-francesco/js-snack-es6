623b0ce33e25f876413871b7690044fe3b2d1492
deca-francesco <francescodecarlo70@gmail.com>
2024-10-11 16:53:30 +0200

    ADD: snack 1

--- Snack_1/snack1.js
+++ Snack_1/snack1.js
@@ -8,3 +8,52 @@ Stampare a schermo la bici con peso minore.
 
 console.log("js linked");
 
+// creo l'array di bici
+const bici_array = [
+    {
+        nome: "Fulmine",
+        peso: 2.5
+    },
+    {
+        nome: "Saetta",
+        peso: 2.7
+    },
+    {
+        nome: "Lampo",
+        peso: 2.8
+    }
+];
+
+console.log(bici_array);
+
+// prendo il div dalla pagina
+const biciEl = document.getElementById("bici");
+
+/*
+// prendo i pesi col ciclo for in
+const pesi_array = [];
+let peso_singolo;
+for (const peso in bici_array) {
+    peso_singolo = bici_array[peso];
+    pesi_array.push(peso_singolo);
+    console.log(peso_singolo);
+}
+console.log(pesi_array);
+*/
+
+// prendo il peso minore
+const peso_minore = Math.min(bici_array[0].peso, bici_array[1].peso, bici_array[2].peso);
+console.log(peso_minore);
+
+// prendo l'oggetto col peso minore
+let leggera;
+
+for (let i = 0; i < bici_array.length; i++) {
+    if (peso_minore == bici_array[i].peso) {
+        leggera = bici_array[i];
+    }
+}
+console.log(leggera);
+
+// metto in pagina
+biciEl.innerHTML = (`<h3>La bici più leggera è: ${leggera.nome} e pesa ${leggera.peso} kg</h3>`);
